821a1048ec86062cee9cfcfdd914d66b6665b42e
uysnon <avgorkin1@gmail.com>
2018-12-03 21:27:07 +0300

    First commit

---
+++ app/src/androidTest/java/labmatr7413/avgor/lab_matr/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package labmatr7413.avgor.lab_matr;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("labmatr7413.avgor.lab_matr", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/labmatr7413/avgor/lab_matr/MainActivity.java
@@ -0,0 +1,13 @@
+package labmatr7413.avgor.lab_matr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
